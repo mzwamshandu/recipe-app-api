46a075013021e8ecf581e4e657e15ff3373bb918
mshandu <mzwandile.shandu@barrowsonline.com>
2019-01-22 21:33:23 +0200

    added command and DB Test but the tests are not working

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,9 +10,9 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None,  {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name', )}),
-        (
-            _('Permissions'), 
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        (_('Permissions'), {'fields': 
+            ('is_active', 'is_staff', 'is_superuser')
+        }
         ),
         (_('Important dates'), {'fields': ('last_login', )})
     )
