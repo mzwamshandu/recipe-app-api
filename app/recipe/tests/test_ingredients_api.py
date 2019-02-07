764ef22cab0c5a7264e82aee9f015424a50250e7
mshandu <mzwandile.shandu@barrowsonline.com>
2019-02-07 12:40:11 +0200

    add preforme create and two more test

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -42,13 +42,13 @@ class PrivateIngredientsApiTests(TestCase):
 
         ingredient = Ingredient.objects.all().order_by('-name')
         serializer = IngredientSerializer(ingredient, many=True)
-        self.assetEqual(res.status_code, status.HTTP_200_OK)
-        self.assetEqual(res.data, serializer.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
     
     def test_ingredient_limited_to_user(self):
         # Test the ingredients for the authenticated user are returned
         user2 = get_user_model().objects.create_user(
-            'test@appdev.com',
+            'test2@appdev.com',
             'testpassword'
         )
         Ingredient.objects.create(user=user2, name='Vinegar')
@@ -56,6 +56,25 @@ class PrivateIngredientsApiTests(TestCase):
 
         res = self.client.get(INGREDIENTS_URL)
 
-        self.assetEqual(res.status_code, status.HTTP_200_OK)
-        self.assetEqual(len(res.data), 1)
-        self.assetEqual(res.data[0]['name'], ingredient.name)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        # Test create a new ingredient
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name'],
+        ).exists()
+
+        self.assertTrue(exists)
+    
+    def test_create_ingredient_invalid(self):
+        # Test creating invalid ingredient fails
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
