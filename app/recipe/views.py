eb8bf8461f341da60d1f674514e4624a3fc33d0c
mshandu <mzwandile.shandu@barrowsonline.com>
2019-02-07 11:14:12 +0200

    Add test for creating tag and update view with perform create tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+                mixins.ListModelMixin,
+                mixins.CreateModelMixin):
     # Manage tags in the database
     authentication_classes = (TokenAuthentication, )
     permission_classess = (IsAuthenticated, )
@@ -15,3 +17,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         # return object for the current authenticated user only
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        # create a new tag
+        serializer.save(user=self.request.user)
