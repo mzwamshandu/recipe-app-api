5228f013ae1854e0f83a5337f81a302733cff1d4
mshandu <mzwandile.shandu@barrowsonline.com>
2019-02-10 14:53:49 +0200

    Added detailed recipe api end point

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None,  {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name', )}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', )})
@@ -24,3 +24,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
