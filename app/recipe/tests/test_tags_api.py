5aae2af366ecf9b2623c26065586cb203dbb1803
mshandu <mzwandile.shandu@barrowsonline.com>
2019-02-21 19:11:34 +0200

    Add filter by tags and ingredients

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
-from core.models import Tag
+from core.models import Tag, Recipe
 from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
